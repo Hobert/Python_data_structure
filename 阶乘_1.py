7035fe5fbc7f8299fdc8932076a8104be2a0bd8d
Hobert <2716412428@qq.com>
2025-01-11 10:16:53 +0800

    1

---
+++ 阶乘_1.py
@@ -0,0 +1,9 @@
+from pythonds.basic.stack import Stack
+# s = Stack()
+# s.push("Akebi")
+# s.push("Komichi")
+# length = len(s.items)
+# print(length)
+# print(s.pop())
+# print(s.items)
+print(ord("A"))
